e630cca3df3cb2a8e184021515e6387a6ea3018c
packagesdev <dev.packages@gmail.com>
2022-02-24 23:24:37 +0100

    Add error code.

--- app_unexpectedly/app_unexpectedly/CUIDataTransform.h
+++ app_unexpectedly/app_unexpectedly/CUIDataTransform.h
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2021, Stephane Sudre
+ Copyright (c) 2021-2022, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -16,6 +16,11 @@
 #import "CUITextModeDisplaySettings.h"
 
 extern NSString * const CUIDataTransformErrorDomain;
+
+typedef NS_ENUM(NSUInteger, CUIDataTransformError)
+{
+    CUIDataTransformUnknownError=1
+};
 
 extern NSString * const CUIGenericAnchorAttributeName;
 
